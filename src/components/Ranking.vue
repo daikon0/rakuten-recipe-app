d902289c9ff072ac713479845acc2dd02785546d
daikon0 <hamada1207.daiki@gmail.com>
2020-04-20 13:21:59 +0900

    レシピカテゴリ一覧取得

---
+++ src/components/Ranking.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li v-for="category in categories" :key="category.categoryId">
+        <router-link :to="{name:'category', params:{id:category.categoryId}}">{{ category.categoryName }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const axios = require('axios')
+export default {
+  name: 'HelloWorld',
+  data: () => ({
+    categories: []
+  }),
+  created: async function () {
+    try {
+      const category = await axios.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1089766391646982288&categoryType=large')
+      this.categories = category.data.result.large
+    } catch (err) {
+      alert(JSON.stringify(err))
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Category.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="category">
+    <h2>HELLO</h2>
+    <ranking/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Ranking from '@/components/Ranking.vue'
+
+export default {
+  name: 'Category',
+  components: {
+    Ranking
+  }
+}
+
+</script>
